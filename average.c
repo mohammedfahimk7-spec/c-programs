6b5194f8ff31a02b0378563e5eaddecf18de5b4e
mohammedfahimk7-spec <mohammedfahimk7@gmail.com>
2026-06-25 17:08:57 +0530

    cprogram

--- average.c
+++ average.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void){
-    int num1, num2, num3, average;
+    float num1, num2, num3, average;
     printf("Enter 3 numbers : ");
-    scanf("%d %d %d", &num1 , &num2, &num3);
+    scanf("%f %f %f", &num1 , &num2, &num3);
     average = (num1 + num2 +num3)/3;
-    printf("average is %d\n",average);
+    printf("average is %f\n",average);
   return 0;
     
 }
