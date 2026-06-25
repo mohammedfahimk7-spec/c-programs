b10edf80d12cbb0daf19479aa73c7771d2a6f7a6
mohammedfahimk7-spec <mohammedfahimk7@gmail.com>
2026-06-25 16:45:47 +0530

    cprogram

---
+++ minus.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2, sum;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 - num2;
+
+    printf("Sum is %d\n", sum);
+
+    return 0;
+}
